dc7b1e7bb3b6ddc5f7fee2760660a00f266e59dc
JorgeElias0 <jorge-zero@outlook.com>
2019-11-20 00:32:37 -0600

    index

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer>
+                <div class="informacion">
+                    <div class="seccfoot">
+                        <img src="/imagenes/newlogo.png" alt="logo empresa" class="logo">
+                        <p>
+                            <i>
+                                En TAIM nos vamos a asegurar que usted 
+                                siempre este a <b>tiempo</b>.
+                            </i>
+                        </p>
+                    </div>
+                    <div class="seccfoot">
+                        <label>Puedes contactarnos en:</label>
+                        <ul>
+                            <li>Nuestro correo electronico: <a href="#"><i class="fas fa-envelope"></i> TIAM@gmail.com</a></li>
+                            <li>Nuestro numero telefonico: <a href="#"><i class="fas fa-mobile-alt"></i> 444-128-9736</a></li>
+                            <li>Nuestra pagina de Facebook: <a href="#"><i class="fab fa-facebook-f"></i> TIAM SLP</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang = "es">
+    <head>
+        <title>BusCheck</title>
+        <link href="/css/estilo.css" rel=stylesheet>
+        <link href="/css/footer.css" rel=stylesheet>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        <div class="seccion">
+            <img src="imagenes/newlogo.png" alt="logo empresa" class="logo">
+            <a href="html/Logeo.html" class="accesos">Accede A Tu Cuenta</a>
+            <a href="html/Registrate.html" class="accesos">Crea Tu Cuenta</a>
+            <a href="html/Logeo.html" class="accesos">Acerca de Nosotros</a>
+        </div>
+
+<?php
+    include('footer.php');
+?>
