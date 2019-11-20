20c933f76b3b246e0d446076086dba0945797e03
JorgeElias0 <jorge-zero@outlook.com>
2019-11-20 00:35:17 -0600

    logeo

---
+++ logeo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang = "es">
+    <head>
+        <title>BusCheck</title>
+        <link href="/css/estilo.css" rel=stylesheet>
+        <link href="/css/footer.css" rel=stylesheet>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        <div class="seccion">
+            <img src="/imagenes/newlogo.png" alt="logo empresa" class="logo">
+            <h2>¡Inicia Sesion en tu cuenta!</h2>
+            <label >Usuario</label>
+            <input type="text" class="dat"> 
+            <label>Contraseña</label>
+            <input type="password" class="dat">
+            <a href="/html/mapa.html" class="accesos">Acceder</a>
+        </div>
+
+<?php
+    include('footer.php');
+?>
